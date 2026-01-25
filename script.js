7d7b5de39b9e256aad49013bb2570680cbfbeaa3
CodeWithVenu <venusvenu91456@gmail.com>
2026-01-25 23:28:20 +0530

    Fix formatting of showForm function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function showForm(formId) {
     document.querySelectorAll('.form-box').forEach(form => form.classList.remove('active'));
     document.getElementById(formId).classList.add('active');
+ 
 } 
