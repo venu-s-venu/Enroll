f4802169c67dbdb147dbb3be73710eaabc86ca02
CodeWithVenu <venusvenu91456@gmail.com>
2026-01-28 23:00:33 +0530

    Update user_page.php

--- user_page.php
+++ user_page.php
@@ -4,7 +4,7 @@ session_start();
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
+<head> 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Page</title>
@@ -42,3 +42,4 @@ session_start();
 </body>
 
 </html>
+
