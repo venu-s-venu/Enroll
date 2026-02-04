9d7452241fb7769592814070a1ced5fbc968fe03
CodeWithVenu <venusvenu91456@gmail.com>
2026-02-04 23:59:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function showForm(formId) { 
+function showForm(formId) {  
     document.querySelectorAll('.form-box').forEach(form => form.classList.remove('active'));
     document.getElementById(formId).classList.add('active');
  
 } 
+
 
